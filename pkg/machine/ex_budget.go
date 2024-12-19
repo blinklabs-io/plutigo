50c064719d70af203fca9a6626f190997945d705
rvcas <x@rvcas.dev>
2024-12-18 21:54:06 -0500

    feat: compute Var terms

---
+++ pkg/machine/ex_budget.go
@@ -0,0 +1,16 @@
+package machine
+
+type ExBudget struct {
+	mem int64
+	cpu int64
+}
+
+func (ex *ExBudget) occurrences(n uint32) {
+	ex.mem *= int64(n)
+	ex.cpu *= int64(n)
+}
+
+var DefaultExBudget = ExBudget{
+	mem: 14000000,
+	cpu: 10000000000,
+}
---
+++ pkg/machine/value.go
@@ -0,0 +1,3 @@
+package machine
+
+type Value interface{}
