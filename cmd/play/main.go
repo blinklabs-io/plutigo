b861102e32dd9ac7af9983c456066782c9023d37
rvcas <x@rvcas.dev>
2025-05-04 20:09:20 -0400

    chore: support formatting in play CLI

--- cmd/play/main.go
+++ cmd/play/main.go
@@ -15,36 +15,58 @@ func main() {
 		os.Exit(1)
 	}
 
-	filename := os.Args[1]
+	var filename string
+	var format bool
 
-	content, _ := os.ReadFile(filename)
+	if os.Args[1] == "-f" {
+		filename = os.Args[2]
+		format = true
+	} else {
+		filename = os.Args[1]
+		format = false
+	}
+
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("loading file error: %v\n\n", err)
+
+		os.Exit(1)
+	}
 
 	input := string(content)
 
 	program, err := syn.Parse(input)
 	if err != nil {
-		fmt.Printf("parse error: %v", err)
+		fmt.Printf("parse error: %v\n\n", err)
 
 		os.Exit(1)
 	}
 
-	dProgram, err := syn.NameToNamedDeBruijn(program)
-	if err != nil {
-		fmt.Printf("conversion error: %v", err)
+	if !format {
+		dProgram, err := syn.NameToNamedDeBruijn(program)
+		if err != nil {
+			fmt.Printf("conversion error: %v\n\n", err)
 
-		os.Exit(1)
+			os.Exit(1)
+		}
+
+		machine := cek.NewMachine[syn.NamedDeBruijn](200)
+
+		term, err := machine.Run(dProgram.Term)
+		if err != nil {
+			fmt.Printf("eval error: %v\n\n", err)
+
+			os.Exit(1)
+		}
+
+		prettyTerm := syn.PrettyTerm[syn.NamedDeBruijn](term)
+
+		fmt.Println(prettyTerm)
+	} else {
+		prettyProgram := syn.Pretty[syn.Name](program)
+
+		os.WriteFile(filename, []byte(prettyProgram), 0644)
+
+		fmt.Println("done.")
 	}
-
-	machine := cek.NewMachine[syn.NamedDeBruijn](200)
-
-	term, err := machine.Run(dProgram.Term)
-	if err != nil {
-		fmt.Printf("eval error: %v", err)
-
-		os.Exit(1)
-	}
-
-	prettyTerm := syn.PrettyTerm[syn.NamedDeBruijn](term)
-
-	fmt.Println(prettyTerm)
 }
