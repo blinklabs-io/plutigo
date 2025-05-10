f784a247ac5622e7a27cfe91f7a1414a5c0a71d6
rvcas <x@rvcas.dev>
2025-05-10 01:17:30 -0400

    chore: flat encode/decode API
    Signed-off-by: rvcas <x@rvcas.dev>

---
+++ pkg/syn/flat_decode.go
@@ -0,0 +1,5 @@
+package syn
+
+func Decode[T Binder](bytes []byte) (*Program[T], error) {
+	return nil, nil
+}
---
+++ pkg/syn/flat_encode.go
@@ -0,0 +1,5 @@
+package syn
+
+func Encode[T Binder](program *Program[T]) ([]byte, error) {
+	return nil, nil
+}
