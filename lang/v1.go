4de2a5f94b65348f913e553de491b50b7ccd0cd6
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-08-26 12:33:04 -0400

    docs(lang): correct Plutus parameter sources
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- lang/v1.go
+++ lang/v1.go
@@ -3,7 +3,7 @@ package lang
 // Cost model param names for V1
 // These correspond to string keys in the cost models in alonzo-genesis.json, as well as define
 // the ordering for cost models params provided as a list of integers
-// NOTE: this is generated from https://github.com/IntersectMBO/plutus/blob/aee28e0467be0561de5c2f097f639914d8d7a294/plutus-ledger-api/src/PlutusLedgerApi/V1/ParamName.hs
+// NOTE: this is generated from https://github.com/IntersectMBO/plutus/blob/3109dc1e6501ecbbed0b7ae27361c255d7a16173/plutus-ledger-api/src/PlutusLedgerApi/V1/ParamName.hs
 var CostModelParamNamesV1 = []string{
 	"addInteger-cpu-arguments-intercept",
 	"addInteger-cpu-arguments-slope",
--- lang/v2.go
+++ lang/v2.go
@@ -3,7 +3,7 @@ package lang
 // Cost model param names for V2
 // These correspond to string keys in the cost models in alonzo-genesis.json, as well as define
 // the ordering for cost models params provided as a list of integers
-// NOTE: this is generated from https://github.com/IntersectMBO/plutus/blob/aee28e0467be0561de5c2f097f639914d8d7a294/plutus-ledger-api/src/PlutusLedgerApi/V2/ParamName.hs
+// NOTE: this is generated from https://github.com/IntersectMBO/plutus/blob/3109dc1e6501ecbbed0b7ae27361c255d7a16173/plutus-ledger-api/src/PlutusLedgerApi/V2/ParamName.hs
 var CostModelParamNamesV2 = []string{
 	"addInteger-cpu-arguments-intercept",
 	"addInteger-cpu-arguments-slope",
